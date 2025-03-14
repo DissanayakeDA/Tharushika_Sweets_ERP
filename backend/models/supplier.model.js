0c5ba11b2dc0feac3b5544c57a7a187a0a9655cf
Dumindu Dissanayake <duminduad@icloud.com>
2025-03-14 15:38:22 +0530

    Add initial  definitions and configuration files for various modules

---
+++ backend/models/supplier.model.js
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+
+const supplierSchema = new mongoose.Schema(
+  {
+    supplier_name: {
+      type: String,
+      required: true,
+    },
+    supplier_address: {
+      type: String,
+      required: true,
+    },
+    supplier_phone: {
+      type: String,
+      required: true,
+    },
+    supplier_email: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Supplier = mongoose.model("Supplier", supplierSchema);
+
+export default Supplier;
---
+++ backend/routes/product.route.js
@@ -0,0 +1,18 @@
+import {
+  addProduct,
+  deleteProduct,
+  getAllProduct,
+  getProductById,
+  updateProduct,
+} from "../controllers/product.controller.js";
+import express from "express";
+
+const router = express.Router();
+
+router.post("/", addProduct);
+router.get("/:id", getProductById);
+router.get("/", getAllProduct);
+router.put("/:id", updateProduct);
+router.delete("/:id", deleteProduct);
+
+export default router;
