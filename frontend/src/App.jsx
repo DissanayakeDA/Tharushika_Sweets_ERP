2afddd0cc05f78833531a3a3edadd8875e1d0d5a
Dumindu Dissanayake <duminduad@icloud.com>
2025-03-18 12:42:37 +0530

    Remove Stocks and AddStockSelect components; implement updateStock route and supplier management features

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,7 +7,6 @@ import AddStock from "@/Component/AddStock/AddStock";
 import IssueItems from "@/Component/IssueItems/IssueItems";
 import AddBuyers from "@/Component/AddBuyers/AddBuyers";
 import SignOut from "@/Component/SignOut/SignOut";
-import Stocks from "@/Component/Stocks/Stocks";
 import Buyers from "@/Component/Buyers/Buyers";
 import Sales from "@/Component/Sales/Sales";
 import Requests from "@/Component/Requests/Requests";
@@ -16,8 +15,10 @@ import Invoice from "@/Component/Invoice_DBuyers/Invoice";
 import DirectBuyerReturns from "./Component/DirectBuyerReturns/DirectBuyerReturns";
 import AddSuppliers from "./Component/AddSupplier/AddSupplier";
 import HomeGM from "./Component/GMDashboard/GMDashboard";
-import ReturnInvoice from './Component/ReturnInvoice/ReturnInvoice';
-import ViewReturns from './Component/ViewReturns/ViewReturns';
+import ReturnInvoice from "./Component/ReturnInvoice/ReturnInvoice";
+import ViewReturns from "./Component/ViewReturns/ViewReturns";
+import ViewStock from "./Component/ViewStock/ViewStock";
+import Suppliers from "./Component/Suppliers/Suppliers";
 
 function App() {
   return (
@@ -30,7 +31,6 @@ function App() {
           <Route path="/issueitems" element={<IssueItems />} />
           <Route path="/addbuyers" element={<AddBuyers />} />
           <Route path="/signout" element={<SignOut />} />
-          <Route path="/viewstocks" element={<Stocks />} />
           <Route path="/viewbuyers" element={<Buyers />} />
           <Route path="/viewsales" element={<Sales />} />
           <Route path="/viewrequests" element={<Requests />} />
@@ -39,9 +39,10 @@ function App() {
           <Route path="/directreturns" element={<DirectBuyerReturns />} />
           <Route path="/addsuppliers" element={<AddSuppliers />} />
           <Route path="/home-gm" element={<HomeGM />} />
-            <Route path="/returninvoice" element={<ReturnInvoice/>}/>
-          <Route path="/viewReturns" element={<ViewReturns/>}/>
-
+          <Route path="/returninvoice" element={<ReturnInvoice />} />
+          <Route path="/viewReturns" element={<ViewReturns />} />
+          <Route path="/viewstock" element={<ViewStock />} />
+          <Route path="/viewsuppliers" element={<Suppliers />} />
         </Routes>
       </React.Fragment>
     </div>
